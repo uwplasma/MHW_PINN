f13514b31c15661ae768c3178a9b5bf02c01067a
katiejoslyn <k_joslyn@coloradocollege.edu>
2025-03-24 07:12:51 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,7 @@
+git clone https://github.com/katiejoslyn/MHW_PINN.git
+cd MHW_PINN
+
+mkdir data models loss training visualizations
+touch models/mhw_network.py loss/physics_loss.py training/train.py visualizations/loss_plot.py visualizations/animate.py requirements.txt README.md
+
+
